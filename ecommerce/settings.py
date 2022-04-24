aa4769cdf973ee4431ac06e55665b192293ff765
Lucas Ferreira Teodoro <lucasfteo@gmail.com>
2022-04-24 17:09:17 -0300

    .

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1!an^1=a+l!w%wrkma-7&)+i4a9%4u=jux%#j2c462xyx9^&1#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
--- store/views.py
+++ store/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 from .models import *
 
 # Create your views here.
@@ -36,3 +37,7 @@ def checkout(request):
         'items':items, 'order':order
     }
     return render(request, 'store/checkout.html', context)
+
+
+def updateItem(request):
+    return JsonResponse('Item was added', safe=False)
